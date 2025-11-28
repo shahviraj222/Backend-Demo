267b4f8e79afa53114ec56f286f941cd4df62276
Aditya More <aditya1875more@gmail.com>
2025-11-28 21:43:29 +0530

    fixed some small middleware issues

--- src/routes/profiles.ts
+++ src/routes/profiles.ts
@@ -1,20 +1,30 @@
 import { Router } from 'express'
 
 import ProfilesApi from '../controllers/profilesController'
+import middlewares from '../middlewares';
 
 const router = Router()
 
-router.get('/', ProfilesApi.getProfiles)
-router.get('/:id', ProfilesApi.getProfile)
-router.post('/', ProfilesApi.createProfile)
-router.put('/:id', ProfilesApi.updateProfile)
-router.delete('/:id', ProfilesApi.deleteProfile)
-router.get('/email/:email', ProfilesApi.getProfileByEmail)
-router.get('/phone/:phone', ProfilesApi.getProfileByPhone)
-router.get('/guests', ProfilesApi.getGuestProfiles)
-router.get('/full', ProfilesApi.getFullProfiles)
-router.get('/me', ProfilesApi.getCurrentProfile)
-router.put('/me', ProfilesApi.updateCurrentProfile)
-router.delete('/me', ProfilesApi.deleteCurrentProfile)
+router.get('/', middlewares.checkForUser, ProfilesApi.getProfiles);
+
+router.get('/:id', middlewares.checkForUser, ProfilesApi.getProfile);
+
+// Allow account creation WITHOUT login
+router.post('/', ProfilesApi.createProfile);
+
+// These should also require login (but you can decide)
+router.put('/:id', middlewares.checkForUser, ProfilesApi.updateProfile);
+router.delete('/:id', middlewares.checkForUser, ProfilesApi.deleteProfile);
+
+router.get('/email/:email', middlewares.checkForUser, ProfilesApi.getProfileByEmail);
+router.get('/phone/:phone', middlewares.checkForUser, ProfilesApi.getProfileByPhone);
+
+router.get('/guests', middlewares.checkForUser, ProfilesApi.getGuestProfiles);
+router.get('/full', middlewares.checkForUser, ProfilesApi.getFullProfiles);
+
+// CURRENT LOGGED-IN USER
+router.get('/me', middlewares.checkForUser, ProfilesApi.getCurrentProfile);
+router.put('/me', middlewares.checkForUser, ProfilesApi.updateCurrentProfile);
+router.delete('/me', middlewares.checkForUser, ProfilesApi.deleteCurrentProfile);
 
 export default router
